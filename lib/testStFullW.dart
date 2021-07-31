8753a8020a1184a4126f4f93ef3f12cf67a8846f
odaykarem <oday.krayem.997@gmail.com>
2021-07-31 20:54:07 +0300

    31-7

--- lib/testStFullW.dart
+++ lib/testStFullW.dart
@@ -8,14 +8,49 @@ class Test extends StatefulWidget {
 }
 
 class TestState extends State<Test> {
-  var text = "How are u ";
+  var selectedCountry;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(),
         drawer: Drawer(),
         body: Center(
-          child: Text("jj"),
+          child: Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            padding: EdgeInsets.all(10),
+            color: Colors.indigo,
+            child: DropdownButton(
+              icon: Icon(
+                Icons.arrow_circle_down_rounded,
+                color: Colors.purple,
+                size: 30,
+              ),
+              // iconSize: 40,
+              // iconEnabledColor: Colors.red,
+              underline: Divider(
+                color: Colors.indigo,
+                thickness: 1,
+              ),
+              // isExpanded: true,
+              dropdownColor: Colors.indigo[200],
+              hint: Text("Choose country"),
+              items: [
+                "USA",
+                "Canada",
+              ]
+                  .map((e) => DropdownMenuItem(
+                        child: Text("$e"),
+                        value: e,
+                      ))
+                  .toList(),
+              onChanged: (val) {
+                setState(() {
+                  selectedCountry = val;
+                });
+              },
+              value: selectedCountry,
+            ),
+          ),
         ));
   }
 }
